c4270067181bcb47f19c338c27f7999e1c1f51c2
kyleliu11111 <45304305+kyleliu11111@users.noreply.github.com>
2018-11-26 14:49:53 -0800

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -10,30 +10,36 @@ typedef pair <int, int> pp;
 
 const int MAXN = 1e5 + 1;
 
-int N, M;
-vector <pp> g[MAXN];
-int d[MAXN], vis[MAXN];
-
+template <int SZ> struct Dijkstra {
+    int d[SZ], vis[SZ];
+    vector <pp> g[SZ];
+    
+    void addEdge(int u, int v, int c) { g[u].PB(pp(v, c)); }
+                                                
+    void go(int s) {
+        priority_queue <pp, vector<pp>, greater<pp> > PQ;
+        
+        PQ.push(MP(0, s));
+        while(PQ.size()) {
+            int u = PQ.top().B;
+            int di = PQ.top().A;
+            PQ.pop();
+            if(vis[u]) continue;
+            vis[u] = 1; d[u] = di;
+            for(pp e : g[u]) if(!vis[e.A]) PQ.push(MP(e.A,  di + e.B)); 
+        }
+    }
+};
 int main(void)
 {
+    int N, M;
+    Dijkstra <MAXN> SP;
     cin >> N >> M;
     for(int i=0; i<M; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        g[a].PB(pp(b, c));
+        int a, b, c; cin >> a >> b >> c;
+        SP.addEdge(a, b, c);
     }
+    SP.go(1);
     
-    priority_queue <pp, vector<pp>, greater<pp> > PQ;
-    
-    PQ.push(MP(0, 1));
-    while(PQ.size()) {
-        int u = PQ.top().B;
-        int di = PQ.top().A;
-        PQ.pop();
-        if(vis[u]) continue;
-        vis[u] = 1; d[u] = di;
-        for(pp e : g[u]) if(!vis[e.A]) PQ.push(MP(e.A,  di + e.B)); 
-    }
-    
-    cout << d[N] << '\n';
+    cout << SP.d[N] << '\n';
 }
