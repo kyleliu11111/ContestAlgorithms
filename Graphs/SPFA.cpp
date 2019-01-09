34b697106ae6f13c79e66c6f99bff00dcf4b0af9
kyleliu11111 <45304305+kyleliu11111@users.noreply.github.com>
2019-01-09 01:12:50 -0800

    Update SPFA.cpp

--- Graphs/SPFA.cpp
+++ Graphs/SPFA.cpp
@@ -1,3 +1,23 @@
+/*
+D'Esopo-Pape's Shortest Path Algorithm (Shortest-Path Faster Algorithm)
+Facts:
+- Fast SSSP Algorithm in practice
+- Handles negative-weight edges
+- Typically faster than Dijkstra and Bellman-Ford
+- Unproven Complexity
+How it works:
+- Maintain d[u] -- shortest path from source s->u at all times
+- Repeatedly "relax" paths with edges
+- Maintain a queue of recently relaxed nodes. At each step, pop a node and:
+	- Try relaxing each neighbor
+		- If relaxed, add it to the queue if it's not already in
+Applications:
+- Find the shortest path from s->v in general graph
+- Solve system of difference constraints in large graphs
+Implemented:
+- Find the shortest path from 1->u for u = {2, ..., N} in directed graph
+*/
+
 #include  <bits/stdc++.h>
 
 using namespace std;
